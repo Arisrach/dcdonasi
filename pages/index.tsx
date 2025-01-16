11bb18d503a868126084383b008bcaa9b3f4e6e2
Arisrach <58836075+Arisrach@users.noreply.github.com>
2025-01-16 17:42:53 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
 // pages/index.tsx
 import React from 'react';
-import Header from '../components/Header';
-import Banner from '../components/Banner';
 import Card from '../components/Card';
-import DonasiProgressBar from '../components/DonasiProgressBar';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
 
 const Home: React.FC = () => {
   return (
