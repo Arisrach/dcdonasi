fdae309363a727ea3e9383041e2bb1a630661211
Arisrach <58836075+Arisrach@users.noreply.github.com>
2025-01-16 21:15:09 +0700

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -44,7 +44,7 @@ export default function Card({ title, description, imageUrl }: CardProps) {
 
         {/* Progress bar */}
         <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 mt-2">
-          <div className="bg-yellow-300 h-2.5 rounded-full" style={{ width: '0%' }}></div>
+          <div className="bg-yellow-300 h-2.5 rounded-full" style={{ width: '8%' }}></div>
         </div>
 
         {/* Tombol Donasi */}
